0cfb1ce10a4c4364d5648cd4ea0751c4a938e7be
MSShahzad786 <shahzad4469176@gmail.com>
2025-02-20 22:17:11 +0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,111 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'package:flutter_todolist/onboarding.dart';
-import 'util.dart';
-
-void main() => runApp(App());
-
-class App extends StatefulWidget {
-  App({Key key}) : super(key: key);
-
-  _AppState createState() => _AppState();
+void main() {
+  runApp(const TodoApp());
 }
 
-class _AppState extends State<App> {
-  @override
-  void initState() {
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent, //top bar color
-      ),
-    );
-    // SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
-    super.initState();
-  }
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        canvasColor: CustomColors.GreyBackground,
-        fontFamily: 'rubik',
-      ),
-      home: Onboarding(),
+      title: 'To-Do List',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
     );
   }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Task> _tasks = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _addTask(String title) {
+    if (title.isNotEmpty) {
+      setState(() {
+        _tasks.add(Task(title));
+      });
+      _controller.clear();
+    }
+  }
+
+  void _toggleTask(int index) {
+    setState(() {
+      _tasks[index].isDone = !_tasks[index].isDone;
+    });
+  }
+
+  void _deleteTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(labelText: 'Enter Task'),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () => _addTask(_controller.text),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Checkbox(
+                    value: _tasks[index].isDone,
+                    onChanged: (value) => _toggleTask(index),
+                  ),
+                  title: Text(
+                    _tasks[index].title,
+                    style: TextStyle(
+                      decoration: _tasks[index].isDone
+                          ? TextDecoration.lineThrough
+                          : null,
+                    ),
+                  ),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _deleteTask(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Task {
+  String title;
+  bool isDone;
+  Task(this.title, {this.isDone = false});
 }
